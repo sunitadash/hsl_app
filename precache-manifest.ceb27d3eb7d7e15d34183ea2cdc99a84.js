22fc1282629a6783299ee3f32581c74ac828393e
Sunita Dash <sunita.dash30@gmail.com>
2019-01-18 00:46:50 +0530

    Updates

---
+++ precache-manifest.ceb27d3eb7d7e15d34183ea2cdc99a84.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "da1c0db00034b0ea038b",
+    "url": "/hsl_app/static/js/runtime~main.da1c0db0.js"
+  },
+  {
+    "revision": "1417f546bff5754d755d",
+    "url": "/hsl_app/static/js/main.1417f546.chunk.js"
+  },
+  {
+    "revision": "af0f9f05e50f1023c93c",
+    "url": "/hsl_app/static/js/1.af0f9f05.chunk.js"
+  },
+  {
+    "revision": "1417f546bff5754d755d",
+    "url": "/hsl_app/static/css/main.43eb8397.chunk.css"
+  },
+  {
+    "revision": "1cdf0f20853acd2c13f7987c638b3658",
+    "url": "/hsl_app/index.html"
+  }
+];
